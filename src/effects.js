b7178eb7600e212384238824444fb62075437d32
Paul Coyle <paul@cohesic.com>
2019-04-21 17:30:43 -0600

    Update dependencies, correct effects imports.

--- src/effects.js
+++ src/effects.js
@@ -1,10 +1,2 @@
-import { effects as sagaEffects } from 'redux-saga';
-
-export const call = sagaEffects.call;
-export const take = sagaEffects.take;
-export const spawn = sagaEffects.spawn;
-export const cancel = sagaEffects.cancel;
-export const join = sagaEffects.join;
-
-// eslint-disable-next-line import/namespace
-export const all = sagaEffects.all || (effects => effects);
+import * as sagaEffects from "redux-saga/effects";
+export const { all, call, cancel, join, spawn, take } = sagaEffects;
